f9f71c38ea8307af7ce4ddff1b42d4e739dfe894
15651892005 <15651892005@163.com>
2020-03-22 00:05:58 +0800

    base version

---
+++ HelloSwipeViewWithTabs/Fragment2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HelloSwipeViewWithTabs
+{
+    public class Fragment2:Fragment
+    {
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            //View ignored = base.OnCreateView(inflater, container, savedInstanceState);
+            View rootView = inflater.Inflate(Resource.Layout.fragment2, null);
+            var test =  rootView.FindViewById<TextView>(Resource.Id.test2222);
+            test.Text = "aaa";
+            return rootView    ;
+        }
+    }
+}
---
+++ HelloSwipeViewWithTabs/MainPage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+using System.Collections.Generic;
+
+namespace HelloSwipeViewWithTabs
+{
+    [Activity(Label = "MainPage",MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainPage : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.mainpage);
+
+            // Create your application here
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+
+            AddTab("Tab 1", Resource.Drawable.abc_ab_share_pack_mtrl_alpha, 0);
+            AddTab("Tab 2", Resource.Drawable.abc_action_bar_item_background_material, 1);
+
+           
+        }
+
+        private void AddTab(string tabText, int iconResourceId, int position)
+        {
+            var tab = this.ActionBar.NewTab();
+            tab.SetText(tabText);
+            tab.SetIcon(Resource.Drawable.Icon);
+
+            // must set event handler before adding tab
+            tab.TabSelected += delegate (object sender, ActionBar.TabEventArgs e) {
+                switch (position)
+                {
+                    case 0:
+                        e.FragmentTransaction.Replace(
+                            Resource.Id.content_frame,
+                            new Fragment1());
+                        break;
+                    case 1:
+                        e.FragmentTransaction.Replace(
+                            Resource.Id
+                                .content_frame,
+                            new Fragment2());
+                        break;
+
+                }
+            };
+
+            this.ActionBar.AddTab(tab);
+        }
+    }
+}
